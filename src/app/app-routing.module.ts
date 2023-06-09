1007234942b68b9fcef38c734acd937fd9768b24
charles Kakai <kaksunchained@gmail.com>
2023-06-09 10:05:23 +0300

    added a routing a module to handle routing for the application.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  {path: 'login ', component: LoginComponent}
+]
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing : true}
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
